2ecdd86c95686ece6dbe6a1ec6797e8e0d27e0d1
guru1234cud <guru2006may@gmail.com>
2025-03-22 11:17:15 +0530

    limited feature

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,34 +1,48 @@
 import React, { useState } from 'react'
 import './App.css'
 import axios from 'axios'
+
+
 const App = () => {
-  const [email,setemail] = useState("")
+  const [email, setemail] = useState("")
+  const [apiresponse, setapiresponse] = useState([])
   const options = {
     method: 'GET',
-    url: 'https://breachdirectory.p.rapidapi.com/',
-    params: {
-      func: 'auto',
-      term: {email}
-    },
+    url: `https://breachsearch.p.rapidapi.com/${email}`,
     headers: {
       'x-rapidapi-key': '1b3198c948msh1eb53f01277c3f2p18bdf1jsnb81ed0e8e4c1',
-      'x-rapidapi-host': 'breachdirectory.p.rapidapi.com'
+      'x-rapidapi-host': 'breachsearch.p.rapidapi.com'
     }
   };
-  
+
   async function fetchData() {
     try {
       const response = await axios.request(options);
       console.log(response.data);
+      fcheck(response.data)
     } catch (error) {
+      setapiresponse([error.message])
+      console.log(apiresponse);
       console.error(error);
     }
   }
+  function fcheck(obj) {
+    setapiresponse(["domain:"+obj.BreachesSummary.domain,"site:"+obj.BreachesSummary.site])
+    console.log(apiresponse)
+  }
   return (
     <div className="main">
-      <input className='inp' onChange={(e)=>setemail(e.target.value)} value={email}></input>
-      <button onClick={fetchData}></button>
+      <h1>Check For Data Breaches</h1>
+      <input className='inp' onChange={(e) => setemail(e.target.value)} value={email}></input>
+      <button onClick={fetchData} className='btn'>PRESS</button>
+      <div className="res">
+        <ul className='keys'>{apiresponse.map((val,index) => {
+          return <li key={index}>{val}</li>
+        })}
+        </ul>
+      </div>
     </div>
+
   )
 }
 
